dc7704f74f0d84b14547d6ee4ac9102e12cc298c
thehoangpth2015 <65445814+thehoangpth2015@users.noreply.github.com>
2022-04-16 22:24:32 +0700

    abcd

---
+++ BT1/Form_dang_ky/register.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <title>Register</title>
+
+    <style>
+        .register{
+            width: 500px;
+            height: fit-content;
+            background-color: antiquewhite;
+            margin: 30px auto;
+            padding: 15px;
+        }
+        h5{
+            text-align: center;
+            width: 100%;
+            margin-bottom: 15px;
+        }
+        .btn_submit{
+            
+        }
+    </style>
+
+</head>
+<body>
+    <div class="register">
+        <form method="POST">
+            <h5>Form Register</h5>
+            <div class="col-auto">
+                <label class="form-label">Name</label>
+                <input type="text" name="fullname" class="form-control">
+            </div>
+            <div class="rgtRow">
+                <label class="form-label">Email</label>
+                <input type="email" name="email" class="form-control">
+            </div>
+            <div class="rgtRow">
+                <label class="form-label">Password</label>
+                <input type="password" name="password" class="form-control">
+            </div>
+            
+            <button type="submit" class="btn btn-outline-success my-3 btn_submit">Register</button>  
+          
+        </form>
+    </div>
+</body>
+</html>
+
+<?php
+echo 'hello';
