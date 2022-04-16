485f6406820bd4d4734e4cb947bcc935327ae604
thehoangpth2015 <65445814+thehoangpth2015@users.noreply.github.com>
2022-04-16 19:54:01 +0700

    abc

--- Test01/test01.php
+++ Test01/test01.php
@@ -1,5 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    h1 {
+        color: antiquewhite;
+
+    }
+    .abc{
+        width: 100px;
+        height: 10px;
+    }
+</style>
+<body>
+    <h1 style="color: palevioletred">thế hoàng</h1>
+    <h2 style="background-color: aquamarine;">đậu xanh nó</h2>
+    <h3>ối dồi ôi</h3>
+</body>
+</html>
+
 <?php
-echo 'hello';
-echo 'tui';
-echo 'lan 2';
-?>
+    echo 'hello';
+
